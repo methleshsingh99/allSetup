5a77bf6c3cb4d9827920e996d32325e55a15f930
methleshsingh99 <78372517+methleshsingh99@users.noreply.github.com>
2026-01-02 14:35:41 +0530

    Update NODE-EXPORTER.sh to include service start
    Added commands to start and check the status of the Node Exporter service.

--- NODE-EXPORTER.sh
+++ NODE-EXPORTER.sh
@@ -1,3 +1,9 @@
+#! /bin/bash
+sudo -i
+apt update
+
+
+
 #NODE-EXPORTER
 wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
 tar -xf node_exporter-1.5.0.linux-amd64.tar.gz
@@ -22,4 +28,5 @@ EOF
 
 sudo cat /etc/systemd/system/node_exporter.service
 sudo systemctl daemon-reload  && sudo systemctl enable node_exporter
+
 sudo systemctl start node_exporter.service && sudo systemctl status node_exporter.service --no-pager
